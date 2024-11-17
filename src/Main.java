e33734cc7f5d6e03b5ed230f2069986b66a3a62c
RamonJosePinto <95755036+RamonJosePinto@users.noreply.github.com>
2024-11-17 17:41:09 -0300

    Criado arquivo de config para pegar os ips das maquinas

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        String caminhoArquivo = "config.txt";
         Scanner scanner = new Scanner(System.in);
         System.out.print("Informe o ID do processo que deseja utilizar: ");
         int id = scanner.nextInt();
 
-        Map<Integer, String> idParaIp = new HashMap<>();
-            idParaIp.put(1, "192.168.56.1");
-            idParaIp.put(2, "192.168.56.1");
-            idParaIp.put(3, "192.168.56.1");
-            idParaIp.put(4, "192.168.56.1");
-            idParaIp.put(5, "192.168.56.1");
+//        Map<Integer, String> idParaIp = new HashMap<>();
+//            idParaIp.put(1, "192.168.56.1");
+//            idParaIp.put(2, "192.168.56.1");
+//            idParaIp.put(3, "192.168.56.1");
+//            idParaIp.put(4, "192.168.56.1");
+//            idParaIp.put(5, "192.168.56.1");
+
+        Map<Integer, String> idParaIp = null;
+        try {
+            idParaIp = carregarMapaIps(caminhoArquivo);
+        } catch (IOException e) {
+            System.err.println("Erro na execução do processo: " + e.getMessage());
+        }
 
 
         // Filtra os IDs maiores para o processo corrente
@@ -39,4 +50,19 @@ public class Main {
             }
         }
     }
+    public static Map<Integer, String> carregarMapaIps(String caminhoArquivo) throws IOException {
+        Map<Integer, String> idParaIp = new HashMap<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(caminhoArquivo))) {
+            String linha;
+            while ((linha = reader.readLine()) != null) {
+                if (linha.trim().isEmpty() || linha.startsWith("#")) continue; // Ignorar linhas vazias ou comentários
+                String[] partes = linha.split("\\s+"); // Divide por espaços em branco
+                int id = Integer.parseInt(partes[0].trim());
+                String ip = partes[1].trim();
+                idParaIp.put(id, ip);
+            }
+        }
+        return idParaIp;
+    }
+
 }
